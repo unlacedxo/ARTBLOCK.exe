ed554f145bf9782d35cd888eb19f7253fb506982
unlacedxo <lacexinplace@gmail.com>
2026-05-18 19:07:55 +0000

    Reset service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,64 +1,19 @@
-// service-worker.js
-
-const CACHE_NAME = "artblock-exe-v99";
-
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./icons/icon-192x192.png",
-  "./icons/icon-512x512.png"
-];
+const CACHE_NAME = "artblock-exe-v200";
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
-  );
-
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
 
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      return fetch(event.request)
-        .then(networkResponse => {
-
-          const clonedResponse = networkResponse.clone();
-
-          caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request, clonedResponse);
-          });
-
-          return networkResponse;
-        })
-        .catch(() => {
-          return caches.match("./index.html");
-        });
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
